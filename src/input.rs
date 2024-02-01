409cd829c0ec89b65661d9cb098f3468fac0019a
Joshua Jo'Sai-Levin <9204074+j-c-levin@users.noreply.github.com>
2024-02-01 16:14:53 +0000

    touch-jump (should) be working

--- src/input.rs
+++ src/input.rs
@@ -5,9 +5,19 @@ pub struct InputPlugin;
 
 impl Plugin for InputPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, jump);
+        app
+            .add_systems(Update,
+                         (
+                             (jump_space,jump_tap),
+                             apply_jump
+                         ).chain(),
+            )
+            .add_event::<JumpEvent>();
     }
 }
+
+#[derive(Event)]
+struct JumpEvent;
 
 #[derive(Component)]
 pub struct Jump {
@@ -20,8 +30,26 @@ impl Jump {
     }
 }
 
-fn jump(
+fn jump_space(
     keyboard_input: Res<Input<KeyCode>>,
+    mut jump_event_writer: EventWriter<JumpEvent>,
+) {
+    if keyboard_input.just_pressed(KeyCode::Space) {
+        jump_event_writer.send(JumpEvent);
+    }
+}
+
+fn jump_tap(
+    touches: Res<Touches>,
+    mut jump_event_writer: EventWriter<JumpEvent>,
+) {
+    if touches.any_just_pressed() {
+        jump_event_writer.send(JumpEvent);
+    }
+}
+
+fn apply_jump(
+    mut jump_event_reader: EventReader<JumpEvent>,
     mut bird: Query<(&mut Velocity, &Jump), With<Gravity>>,
 ) {
     let Ok((mut velocity, jump)) = bird.get_single_mut() else {
@@ -29,7 +57,7 @@ fn jump(
         return;
     };
 
-    if keyboard_input.just_pressed(KeyCode::Space) {
+    for _ in jump_event_reader.read() {
         velocity.velocity.y = jump.speed;
     }
 }
