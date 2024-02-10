f1a1308c80ec04d579a35196e449e07ff49c8e01
Joshua Jo'Sai-Levin <9204074+j-c-levin@users.noreply.github.com>
2024-02-10 10:06:37 +0000

    add asset metafile

--- src/window.rs
+++ src/window.rs
@@ -6,6 +6,10 @@ impl Plugin for WindowPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(
             DefaultPlugins
+                .set(AssetPlugin {
+                    mode: AssetMode::Processed,
+                    ..default()
+                })
                 .set(ImagePlugin::default_nearest())
                 .set(bevy::prelude::WindowPlugin {
                     primary_window: Some(Window {
